242ecb0a419094c08867fc108dce609925746a79
Ruud Schouten <zzding@live.nl>
2017-11-22 14:49:22 +0100

    Console now saves correctly

--- src/eu/blappole/console/Console.java
+++ src/eu/blappole/console/Console.java
@@ -7,10 +7,13 @@ import java.io.*;
 import java.util.*;
 
 public class Console {
+    private static FileOutputStream out;
+    private static DataOutputStream dos;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean stop = false;
-        while(!stop) {
+        while (!stop) {
             System.out.println("What level should be generated? (1-12)");
             int level = scanner.nextInt();
             while (level < 1 || level > 12) {
@@ -18,15 +21,19 @@ public class Console {
                 System.out.println("What level should be generated? (1-12)");
                 level = scanner.nextInt();
             }
-            generateKoch(level);
+            try { generateKoch(level); }
+            catch (FileNotFoundException e) { e.printStackTrace(); }
         }
     }
 
-    private static void generateKoch(int level) {
+    private static void generateKoch(int level) throws FileNotFoundException {
+        out = new FileOutputStream("edges.txt");
+        dos = new DataOutputStream(out);
         KochFractal koch = new KochFractal();
         koch.setLevel(level);
         koch.addObserver((o, arg) -> {
-            writeToFile((Edge)arg);
+            try { writeToFile((Edge) arg); }
+            catch (IOException ex) { ex.printStackTrace(); }
         });
         koch.generateBottomEdge();
         koch.generateLeftEdge();
@@ -34,20 +41,10 @@ public class Console {
         System.out.println(koch.getNrOfEdges());
     }
 
-    private static void writeToFile(Edge e) {
-        //TODO: Fixe this
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream("edgey.txt");
-            DataOutputStream dos = new DataOutputStream(fos);
-            dos.writeDouble(e.X1);
-            dos.writeDouble(e.X2);
-            dos.writeDouble(e.Y1);
-            dos.writeDouble(e.Y1);
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
+    private static void writeToFile(Edge e) throws IOException {
+        dos.writeDouble(e.X1);
+        dos.writeDouble(e.X2);
+        dos.writeDouble(e.Y1);
+        dos.writeDouble(e.Y1);
     }
 }
